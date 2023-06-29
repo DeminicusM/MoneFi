51dae2f559170ee23b3b4ca4511bb1e86dfa4b95
Deminicus McKinnon <125753916+DeminicusM@users.noreply.github.com>
2023-06-29 17:56:44 -0500

    Update NotesUpdateRequest.cs

--- NET/NotesUpdateRequest.cs
+++ NET/NotesUpdateRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.Notes
+namespace MoneFi.Models.Requests.Notes
 {
     public class NotesUpdateRequest : NotesAddRequest, IModelIdentifier 
     {
